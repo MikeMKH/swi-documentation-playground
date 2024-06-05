b755c79800237debc191cbd5650c506af6aee22a
Mike Harris <MikeMKH@users.noreply.github.com>
2024-06-05 06:00:52 -0500

    add A.9.3

--- clpfd.pl
+++ clpfd.pl
@@ -3,5 +3,11 @@
 % https://www.swi-prolog.org/pldoc/man?section=clpfd
 :- begin_tests(clpfd_example).
 
+test(simple_arithmetic_examples) :-
+  X #= 1+2,
+  assertion(X =:= 3),
+  3 #= Y+2,
+  assertion(Y =:= 1).
+  
 :- end_tests(clpfd_example).
 :- run_tests.
